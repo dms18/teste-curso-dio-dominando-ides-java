18b46903d2a00296d6101f1deda5617da0a46fe9
diego <diegomarquessoare@gmail.com>
2022-10-06 13:56:31 -0300

    first commit

---
+++ src/br/com/dio/primeiro_programa.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class primeiro_programa {
+
+	public static void main(String[] args) {
+		
+		gato gato= new gato();
+		Livros livros=new Livros();
+		System.out.println(gato);
+		System.out.println(livros);
+		/*int a=2;
+		int p=5;
+		System.out.println("hello word"+(a+p));*/
+		
+	}
+
+}
+
+class Livros{
+private String nome;
+private String npag;
+	
+}
